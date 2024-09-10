171d83769fdf65e23578be356790ec5a74dd6d23
Filip Janevski <filip@janevski.dev>
2024-09-10 20:05:18 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Gitea simple setup script
+
+This is a simple setup script for Gitea. Database setup and config isn't provided, you can do it following the [official guide](https://docs.gitea.com/installation/database-prep) (or just use SQLite).
+
+## Requirements
+
+A Linux system with `systemd`
+
+## Usage
+
+First edit the variables `setup.sh` and run the script as `root`.
---
+++ setup.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Set up variable
+export GITEA_VERSION=1.22.2
+export DOMAIN=git.example.com
+
+# Install dependencies
+apt install gnupg2 git unzip nginx certbot python3-certbot-nginx -y
+
+# Add git user
+adduser --system --group --disabled-password --shell /bin/bash --home /home/git --gecos 'Git Version Control' git
+
+# Fetch and set up gitea binary
+wget https://dl.gitea.io/gitea/$GITEA_VERSION/gitea-$GITEA_VERSION-linux-amd64
+cp gitea-1.14.6-linux-amd64 /usr/bin/gitea && chmod 755 /usr/bin/gitea
+mkdir -p /etc/gitea /var/lib/gitea/{custom,data,indexers,public,log}
+chown git:git /etc/gitea /var/lib/gitea/{custom,data,indexers,public,log}
+chmod 750 /var/lib/gitea/{data,indexers,log}
+chmod 770 /etc/gitea
+
+# Copy relevant files
+cp config/nginx.conf /etc/nginx/conf.d/gitea.conf
+cp config/systemd.service /etc/systemd/system/gitea.service
+
+# Set up services
+systemctl daemon-reload
+systemctl enable gitea
+systemctl enable nginx
+systemctl start gitea
+systemctl start nginx
+
+# Update nginx config
+nginx -t
+systemctl restart nginx
+
+# Set up SSL certificate
+certbot --nginx -d $DOMAIN
